9af70ef3d189b32682b195a959b964972c8e14f3
gorkatorres <gorkagarcia01002@gmail.com>
2025-10-16 20:41:50 +0200

    modified:   tests/test_e01_sum_list.py

--- tests/test_e01_sum_list.py
+++ tests/test_e01_sum_list.py
@@ -1,14 +1,5 @@
 import pytest
 from src.numbers import sum_list
-<<<<<<< HEAD
-<<<<<<< HEAD
-# Hola, este es un comentario de ejemplo.
-=======
-#esta linea no la tienes
->>>>>>> a9927ee (	modified:   tests/test_e01_sum_list.py)
-=======
-# Hola este es un comentario de ejemplo
->>>>>>> e24726f (	modified:   tests/__pycache__/test_e01_sum_list.cpython-311-pytest-8.4.2.pyc)
 
 def test_sum_list():
     """Prueba que la suma de [1, 2, 3, 4] sea 10"""
